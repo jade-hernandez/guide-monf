cf41f0fd6465ff4cef61a951737f632f38f7bf46
Jade Hernandez <jade.hernandez88@gmail.com>
2026-08-16 11:17:00 +0200

    chore: remove unused starter UI modules

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,22 +1,11 @@
 import { type ReactNode } from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-import { TooltipProvider } from '../components/ui/tooltip';
 import { UserProvider } from '../context/UserProvider';
-
-const queryClient = new QueryClient();
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-      </UserProvider>
-    </QueryClientProvider>
-  );
+  return <UserProvider>{children}</UserProvider>;
 }
